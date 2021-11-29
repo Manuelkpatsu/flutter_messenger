eb81d29966a114da6b6572a4054daa728b3b85c8
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-29 06:51:54 +0000

    Implement google login

--- lib/view/screens/login_screen.dart
+++ lib/view/screens/login_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermessenger/constants/app_constants.dart';
+import 'package:fluttermessenger/constants/color_constants.dart';
+import 'package:fluttermessenger/core/providers/auth_provider.dart';
+import 'package:fluttermessenger/view/widgets/loading_view.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
+
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -11,9 +18,71 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+    switch (authProvider.status) {
+      case Status.authenticateError:
+        Fluttertoast.showToast(msg: "Sign in fail");
+        break;
+      case Status.authenticateCanceled:
+        Fluttertoast.showToast(msg: "Sign in canceled");
+        break;
+      case Status.authenticated:
+        Fluttertoast.showToast(msg: "Sign in success");
+        break;
+      default:
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          AppConstants.loginTitle,
+          style: TextStyle(color: ColorConstants.primaryColor),
+        ),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: TextButton(
+              onPressed: () async {
+                bool isSuccess = await authProvider.handleSignIn();
+                if (isSuccess) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomeScreen(),
+                    ),
+                  );
+                }
+              },
+              child: const Text(
+                'Sign in with Google',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return const Color(0xffdd4b39).withOpacity(0.8);
+                    }
+                    return const Color(0xffdd4b39);
+                  },
+                ),
+                splashFactory: NoSplash.splashFactory,
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  const EdgeInsets.fromLTRB(30, 15, 30, 15),
+                ),
+              ),
+            ),
+          ),
+          // Loading
+          Positioned(
+            child: authProvider.status == Status.authenticating
+                ? const LoadingView()
+                : const SizedBox.shrink(),
+          ),
+        ],
       ),
     );
   }
