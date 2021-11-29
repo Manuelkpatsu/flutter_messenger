89db004ed405613ea3945926c29e401253386fc3
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-11-29 08:45:50 +0000

    Implement Settings and Full Photo screens

--- lib/view/screens/full_photo_screen.dart
+++ lib/view/screens/full_photo_screen.dart
@@ -24,4 +24,3 @@ class FullPhotoScreen extends StatelessWidget {
     );
   }
 }
-
--- lib/view/screens/settings_screen.dart
+++ lib/view/screens/settings_screen.dart
@@ -1,4 +1,18 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermessenger/constants/app_constants.dart';
+import 'package:fluttermessenger/constants/color_constants.dart';
+import 'package:fluttermessenger/constants/firestore_constants.dart';
+import 'package:fluttermessenger/core/models/user_chat.dart';
+import 'package:fluttermessenger/core/providers/setting_provider.dart';
+import 'package:fluttermessenger/view/widgets/loading_view.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,12 +22,303 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  TextEditingController? controllerNickname;
+  TextEditingController? controllerAboutMe;
+
+  String id = '';
+  String nickname = '';
+  String aboutMe = '';
+  String photoUrl = '';
+
+  bool isLoading = false;
+  File? avatarImageFile;
+  late SettingProvider settingProvider;
+
+  final FocusNode focusNodeNickname = FocusNode();
+  final FocusNode focusNodeAboutMe = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    settingProvider = context.read<SettingProvider>();
+    readLocal();
+  }
+
+  void readLocal() {
+    setState(() {
+      id = settingProvider.getPref(FirestoreConstants.id) ?? "";
+      nickname = settingProvider.getPref(FirestoreConstants.nickname) ?? "";
+      aboutMe = settingProvider.getPref(FirestoreConstants.aboutMe) ?? "";
+      photoUrl = settingProvider.getPref(FirestoreConstants.photoUrl) ?? "";
+    });
+
+    controllerNickname = TextEditingController(text: nickname);
+    controllerAboutMe = TextEditingController(text: aboutMe);
+  }
+
+  Future getImage() async {
+    ImagePicker imagePicker = ImagePicker();
+    XFile? pickedFile =
+        await imagePicker.pickImage(source: ImageSource.gallery).catchError((err) {
+      Fluttertoast.showToast(msg: err.toString());
+    });
+    File? image;
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+    }
+    if (image != null) {
+      setState(() {
+        avatarImageFile = image;
+        isLoading = true;
+      });
+      uploadFile();
+    }
+  }
+
+  Future uploadFile() async {
+    String fileName = id;
+    UploadTask uploadTask = settingProvider.uploadFile(avatarImageFile!, fileName);
+    try {
+      TaskSnapshot snapshot = await uploadTask;
+      photoUrl = await snapshot.ref.getDownloadURL();
+      UserChat updateInfo = UserChat(
+        id: id,
+        photoUrl: photoUrl,
+        nickname: nickname,
+        aboutMe: aboutMe,
+      );
+      settingProvider
+          .updateDataFirestore(
+              FirestoreConstants.pathUserCollection, id, updateInfo.toJson())
+          .then((data) async {
+        await settingProvider.setPref(FirestoreConstants.photoUrl, photoUrl);
+        setState(() {
+          isLoading = false;
+        });
+        Fluttertoast.showToast(msg: "Upload success");
+      }).catchError((err) {
+        setState(() {
+          isLoading = false;
+        });
+        Fluttertoast.showToast(msg: err.toString());
+      });
+    } on FirebaseException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      Fluttertoast.showToast(msg: e.message ?? e.toString());
+    }
+  }
+
+  void handleUpdateData() {
+    focusNodeNickname.unfocus();
+    focusNodeAboutMe.unfocus();
+
+    setState(() {
+      isLoading = true;
+    });
+    UserChat updateInfo = UserChat(
+      id: id,
+      photoUrl: photoUrl,
+      nickname: nickname,
+      aboutMe: aboutMe,
+    );
+    settingProvider
+        .updateDataFirestore(
+            FirestoreConstants.pathUserCollection, id, updateInfo.toJson())
+        .then((data) async {
+      await settingProvider.setPref(FirestoreConstants.nickname, nickname);
+      await settingProvider.setPref(FirestoreConstants.aboutMe, aboutMe);
+      await settingProvider.setPref(FirestoreConstants.photoUrl, photoUrl);
+
+      setState(() {
+        isLoading = false;
+      });
+
+      Fluttertoast.showToast(msg: "Update success");
+    }).catchError((err) {
+      setState(() {
+        isLoading = false;
+      });
+
+      Fluttertoast.showToast(msg: err.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Settings Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          AppConstants.settingsTitle,
+          style: TextStyle(color: ColorConstants.primaryColor),
+        ),
+        centerTitle: true,
       ),
+      body: settingsPage(),
+    );
+  }
+
+  Widget settingsPage() {
+    return Stack(
+      children: <Widget>[
+        SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Avatar
+              CupertinoButton(
+                onPressed: getImage,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  child: avatarImageFile == null
+                      ? photoUrl.isNotEmpty
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(45),
+                              child: Image.network(
+                                photoUrl,
+                                fit: BoxFit.cover,
+                                width: 90,
+                                height: 90,
+                                errorBuilder: (context, object, stackTrace) {
+                                  return const Icon(
+                                    Icons.account_circle,
+                                    size: 90,
+                                    color: ColorConstants.greyColor,
+                                  );
+                                },
+                                loadingBuilder: (BuildContext context, Widget child,
+                                    ImageChunkEvent? loadingProgress) {
+                                  if (loadingProgress == null) return child;
+                                  return SizedBox(
+                                    width: 90,
+                                    height: 90,
+                                    child: Center(
+                                      child: CircularProgressIndicator(
+                                        color: ColorConstants.themeColor,
+                                        value: loadingProgress.expectedTotalBytes !=
+                                                    null &&
+                                                loadingProgress.expectedTotalBytes != null
+                                            ? loadingProgress.cumulativeBytesLoaded /
+                                                loadingProgress.expectedTotalBytes!
+                                            : null,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            )
+                          : const Icon(
+                              Icons.account_circle,
+                              size: 90,
+                              color: ColorConstants.greyColor,
+                            )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(45),
+                          child: Image.file(
+                            avatarImageFile!,
+                            width: 90,
+                            height: 90,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                ),
+              ),
+
+              // Input
+              Column(
+                children: <Widget>[
+                  // Username
+                  Container(
+                    child: const Text(
+                      'Nickname',
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold,
+                          color: ColorConstants.primaryColor),
+                    ),
+                    margin: const EdgeInsets.only(left: 10, bottom: 5, top: 10),
+                  ),
+                  Container(
+                    child: Theme(
+                      data: Theme.of(context)
+                          .copyWith(primaryColor: ColorConstants.primaryColor),
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Sweetie',
+                          contentPadding: EdgeInsets.all(5),
+                          hintStyle: TextStyle(color: ColorConstants.greyColor),
+                        ),
+                        controller: controllerNickname,
+                        onChanged: (value) {
+                          nickname = value;
+                        },
+                        focusNode: focusNodeNickname,
+                      ),
+                    ),
+                    margin: const EdgeInsets.only(left: 30, right: 30),
+                  ),
+
+                  // About me
+                  Container(
+                    child: const Text(
+                      'About me',
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold,
+                          color: ColorConstants.primaryColor),
+                    ),
+                    margin: const EdgeInsets.only(left: 10, top: 30, bottom: 5),
+                  ),
+                  Container(
+                    child: Theme(
+                      data: Theme.of(context)
+                          .copyWith(primaryColor: ColorConstants.primaryColor),
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Fun, like travel and play PES...',
+                          contentPadding: EdgeInsets.all(5),
+                          hintStyle: TextStyle(color: ColorConstants.greyColor),
+                        ),
+                        controller: controllerAboutMe,
+                        onChanged: (value) {
+                          aboutMe = value;
+                        },
+                        focusNode: focusNodeAboutMe,
+                      ),
+                    ),
+                    margin: const EdgeInsets.only(left: 30, right: 30),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+
+              // Button
+              Container(
+                child: TextButton(
+                  onPressed: handleUpdateData,
+                  child: const Text(
+                    'Update',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(ColorConstants.primaryColor),
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    ),
+                  ),
+                ),
+                margin: const EdgeInsets.only(top: 50, bottom: 50),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.only(left: 15, right: 15),
+        ),
+
+        // Loading
+        Positioned(child: isLoading ? const LoadingView() : const SizedBox.shrink()),
+      ],
     );
   }
 }
